c1a11ce6828481c961da45e910a3d60d4d13bc7e
gcinbax <gcinbax@gmail.com>
2015-02-05 15:19:28 +0400

    Buttons work
    Hell yeah

--- src/vkmanager/model/VKTrack.java
+++ src/vkmanager/model/VKTrack.java
@@ -1,18 +1,46 @@
 package vkmanager.model;
 
-import javafx.geometry.Rectangle2D;
+import java.net.URL;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class VKTrack{
+
+    private static int index = 0;
     
+    private int trackIndex; 
     private String name;
-    private boolean playing;    
+    private boolean playing;
     private long lengthSec;
     private ImageView play;
     private ImageView pause;
+    private URL trackUrl;
+    private MediaPlayer player;
+    private Button trackBut;
+    
+
+    public VKTrack(String name, URL trackUrl, Button trackBut){
+        trackIndex = index++;
+        this.name = name;
+        this.trackBut = trackBut;
+        this.playing = false;
+        this.trackUrl = trackUrl;
+        Media mp3 = new Media(trackUrl.toString());
+        player = new MediaPlayer(mp3);
+    }
 
     public ImageView getPlay(){
         return play;
+    }
+
+    public URL getTrackUrl(){
+        return trackUrl;
+    }
+
+    public void setTrackUrl(URL trackUrl){
+        this.trackUrl = trackUrl;
     }
 
     public void setPlay(ImageView play){
@@ -26,21 +54,37 @@ public class VKTrack{
     public void setPause(ImageView pause){
         this.pause = pause;
     }
-    
-    public VKTrack(String name, long lengthSec){
-        this.name = name;
-        this.playing = false;
-        this.lengthSec = lengthSec;
-    }
 
     public boolean isPlaying(){
         return playing;
     }
-    
+
     public void invertStatus(){
+        if (playing){
+            pause();
+        } else {
+            play();
+        }
         playing = !playing;
     }
 
-   
+    public void play(){
+        player.play();
+        trackBut.setGraphic(pause);
+    }
     
+    public void pause(){
+        player.pause();
+        trackBut.setGraphic(play);
+    }
+    
+    public void stop(){
+        player.stop();
+        trackBut.setGraphic(play);
+    }
+    
+    public int getTrackIndex(){
+        return trackIndex;
+    }
+
 }
